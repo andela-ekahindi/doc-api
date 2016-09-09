84147fa28f6aabde35025e4d4911994f9ea09f1e
Esther Dama Kahindi <esther.kahindi@andela.com>
2016-09-09 15:27:39 +0300

    Add async callbacks to seeds.js

--- server/seeder/seeds.js
+++ server/seeder/seeds.js
@@ -4,13 +4,13 @@ var mongodb = require('mongodb');
 var async = require('async');
 var FakeUsers = require('./data/users');
 var FakeRoles = require('./data/roles');
-
-var url = 'mongodb://localhost/test-docs';
+var users, roles;
 
 async.series([
+
     function(callback) {
+
         var MongoClient = mongodb.MongoClient;
-        var users, roles;
         MongoClient.connect(config.db, function(err, docs) {
             if (err) {
                 console.log("Something went wrong:", err);
@@ -23,36 +23,43 @@ async.series([
                 console.log("---------ROLES----------------------");
                 roles.remove();
                 console.log("+++++++++ADDING DATA++++++++++++++++");
-                users.insert(FakeUsers, function(err, result) {
-                    if (err) {
-                        console.log("Something went wrong:", err)
-                    } else {
-                        console.log("---------USERS----------------------");
-                        result.ops.forEach(function(user) {
-                            console.log("USERNAME:", user.username);
-                            console.log("_ID:", user._id);
-                        });
-                        console.log("ADDED", result.insertedCount, "USERS");
-                    }
+                callback();
+
+            }
+        })
+    },
+    function(callback) {
+        users.insert(FakeUsers, function(err, result) {
+            if (err) {
+                console.log("Something went wrong:", err)
+            } else {
+                console.log("---------USERS----------------------");
+                result.ops.forEach(function(user) {
+                    console.log("USERNAME:", user.username);
+                    console.log("_ID:", user._id);
                 });
-                roles.insert(FakeRoles, function(err, result) {
-                    if (err) {
-                        console.log("Something went wrong:", err)
-                    } else {
-                        console.log("---------ROLES----------------------");
-                        result.ops.forEach(function(role) {
-                            console.log("ROLE TITLE:", role.title);
-                            console.log("_ID:", role._id);
-                        });
-                    }
-                    console.log("ADDED", result.insertedCount, "ROLES");
-        callback(); 
-                    
-                });
+                console.log("ADDED", result.insertedCount, "USERS");
+                callback();
 
             }
         });
+    },
 
+    function(callback) {
+        roles.insert(FakeRoles, function(err, result) {
+            if (err) {
+                console.log("Something went wrong:", err)
+            } else {
+                console.log("---------ROLES----------------------");
+                result.ops.forEach(function(role) {
+                    console.log("ROLE TITLE:", role.title);
+                    console.log("_ID:", role._id);
+                });
+            }
+            console.log("ADDED", result.insertedCount, "ROLES");
+            callback();
+
+        });
     }
 ], function(err) {
     if (err)
